11a3eefa2bed485651121bb1b4e9ab44722e1783
Taylor Simpson <tdsimpson23@gmail.com>
2019-10-26 12:10:43 -0400

    Navbar no longer changes colour

--- js/custom.js
+++ js/custom.js
@@ -15,49 +15,9 @@ $(document).ready(function () {
         }
     });
     $('#back-to-top').click(function () {
-        $("html, body").animate({scrollTop: 0}, 600);
+        $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
-
-    /*--
-     Variables
-     -----------------------------------*/
-    var $imagePopup = $('[data-image-popup]');
-
-    /*Image*/
-    $imagePopup.magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        },
-    });
-
-});
-
-$(document).ready(function () {
-    var myButton = $('#mybutton');
-    var userFeed = new Instafeed({
-        get: 'user',
-        userId: '4828631159',
-        accessToken: '4828631159.1677ed0.3e66d0fb39cc4a8383ddd034121c65dc',
-        limit: '6',
-        sortBy: 'most-recent',
-        after: function () {
-            var images = $("#instafeed").find('a');
-            $.each(images, function (index, image) {
-                var delay = (index * 75) + 'ms';
-                $(image).css('-webkit-animation-delay', delay);
-                $(image).css('-moz-animation-delay', delay);
-                $(image).css('-ms-animation-delay', delay);
-                $(image).css('-o-animation-delay', delay);
-                $(image).css('animation-delay', delay);
-                $(image).addClass('animated flipInX');
-            });
-
-        },
-        template: ' <div class="col-md-4 col-sm-6 col-xs-4"><div class="insta-image"><a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a></div></div>'
-    });
-    userFeed.run();
 });
 
 
